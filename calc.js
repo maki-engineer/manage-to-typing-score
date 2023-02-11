4e4d92622897bb8db23bf52695f4f86e4e8ec631
maki-engineer <ryohei-81@outlook.jp>
2023-02-11 19:58:39 +0900

    update: タイピングのスコアを計算するプログラムを作成

---
+++ calc.js
@@ -0,0 +1,36 @@
+// 打／秒とミス数を受け取って、wpmとスコアを計算して出力する。
+
+// スコア計算の関数
+function scoreCalc(perSec, miss){
+  let wpm            = perSec * 60;
+  let correctPercent = 400 / (400 + miss);
+  correctPercent   **= 3;
+  let result         = Math.floor(wpm * correctPercent);
+
+  return [Math.floor(wpm), result];
+}
+
+console.log("\n打／秒とミス数をスペース区切りで入力してエンターキーを押してください。");
+
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.on("line", input => {
+  const data   = input.split(" ");
+  const perSec = parseFloat(data[0]);
+  const miss   = Number(data[1]);
+
+  const wpmAndScore = scoreCalc(perSec, miss);
+
+  console.log("\n--------------------------------------------------");
+  console.log("|                                                |");
+  console.log("| wpm   ： " + wpmAndScore[0] + "                                   |");
+  console.log("|                                                |");
+  console.log("| スコア： " + wpmAndScore[1] + "                                   |");
+  console.log("|                                                |");
+  console.log("--------------------------------------------------");
+
+  reader.close();
+});
