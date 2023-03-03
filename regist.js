5723328c2548578a271316e4c7b9fd4be0074e13
maki-engineer <ryohei-81@outlook.jp>
2023-03-03 10:55:45 +0900

    update: 平均値を記録する機能を廃止、同じ日に記録しようとしたら記録できないように設定

--- regist.js
+++ regist.js
@@ -29,13 +29,11 @@ let formatMonth = today_month.toString().padStart(2, "0");
 let formatDate  = today_date.toString().padStart(2, "0");
 let fileName    = formatYear + formatMonth + formatDate + "t.txt";
 let today       = today_month + "月" + today_date + "日";
-let isSearch    = false;
 
 fs.readdir("E:/タイプウェル国語R/JR全履歴", (err, files) => {
   files.forEach(file => {
       if(file === fileName){
         let highScores;
-        isSearch       = true;
         let highScore  = 0;
         let text       = fs.readFileSync("E:/タイプウェル国語R/JR全履歴/" + file);
         let lines      = text.toString().split("\n");
@@ -60,7 +58,12 @@ fs.readdir("E:/タイプウェル国語R/JR全履歴", (err, files) => {
 
           while(true){
             if(workbook.sheet("タイピング").cell("B" + String(rowIndex)).value()){
-              rowIndex++;
+              // すでに記録されてたら記録しないように
+              if(workbook.sheet("タイピング").cell("B" + String(rowIndex)).value() === today){
+                break;
+              }else{
+                rowIndex++;
+              }
             }else{
               // 新しく記録(row_index の行に結果を記録)
               workbook.sheet("タイピング").cell("B" + String(rowIndex)).value(today);
@@ -79,53 +82,10 @@ fs.readdir("E:/タイプウェル国語R/JR全履歴", (err, files) => {
             console.log("タイピングのスコアをエクセルとテーブルに記録しました！");
           });
         });
+
+        setTimeout(() => {
+          return;
+        }, 5_000);
       }
   });
-
-  // もしタイプウェルが出来なかったら、今までのスコアなどの平均を記録しておく
-  if(!isSearch){
-    xlsxPopulate.fromFileAsync("data.xlsx").then(workbook => {
-      let rowIndex = 3;
-      let aveWpm   = 0;
-      let aveMiss  = 0;
-      let avePerSec;
-      let aveScore;
-
-      while(true){
-        // 各日のスコアなどを足していく
-        if(workbook.sheet("タイピング").cell("B" + String(rowIndex)).value()){
-          aveWpm   += workbook.sheet("タイピング").cell("C" + String(rowIndex)).value();
-          aveMiss  += workbook.sheet("タイピング").cell("D" + String(rowIndex)).value();
-
-          rowIndex++;
-        }else{
-          // 各値の平均値を計算
-          aveWpm   = Math.round(aveWpm / (rowIndex - 3));
-          aveMiss  = Math.round(aveMiss / (rowIndex - 3));
-
-          avePerSec = Math.round((aveWpm / 60) * 100) / 100;
-          aveScore  = scoreCalc(avePerSec, aveMiss);
-
-          // 平均値を新しく記録(row_index の行に結果を記録)
-          workbook.sheet("タイピング").cell("B" + String(rowIndex)).value(today);
-          workbook.sheet("タイピング").cell("C" + String(rowIndex)).value(aveWpm);
-          workbook.sheet("タイピング").cell("D" + String(rowIndex)).value(aveMiss);
-          workbook.sheet("タイピング").cell("E" + String(rowIndex)).value(aveScore[1]);
-
-          // テーブルにデータ追加
-          db.run("insert into scores(date, wpm, miss, score) values(?, ?, ?, ?)", today, aveWpm, aveMiss, aveScore[1]);
-          break;
-        }
-      }
-    
-      // 上書き保存
-      workbook.toFileAsync("data.xlsx").then(result => {
-        console.log("今日はタイプウェルをやっていなかったため、今までのタイピングのスコアの平均をエクセルとテーブルに記録しました！");
-      });
-    });
-  }
 });
-
-setTimeout(() => {
-  return;
-}, 5_000);
